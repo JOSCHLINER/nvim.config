964d072eea97fc449f9ca49df98c917a095164c3
joschliner <joschliner@noreply.users.github.com>
2024-08-23 07:43:17 +0000

    added harpoon and a debugger

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -369,6 +369,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sp', builtin.git_files, { desc = '[S]earch [P]roject Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -892,7 +893,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -915,5 +916,6 @@ require('lazy').setup({
   },
 })
 
+require 'custom.keymaps'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,81 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      require('harpoon').setup()
+
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon [A]dd file' })
+      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = 'Harpoon view marked files' })
+
+      vim.keymap.set('n', '<C-h>', function()
+        ui.nav_file(1)
+      end, { desc = 'Harpoon navigate to file 1' })
+      vim.keymap.set('n', '<C-j>', function()
+        ui.nav_file(2)
+      end, { desc = 'Harpoon navigate to file 2' })
+      vim.keymap.set('n', '<C-k>', function()
+        ui.nav_file(3)
+      end, { desc = 'Harpoon navigate to file 3' })
+      vim.keymap.set('n', '<C-l>', function()
+        ui.nav_file(4)
+      end, { desc = 'Harpoon navigate to file 4' })
+
+      vim.keymap.set('n', '<C-p>', ui.nav_prev, { desc = 'Harpoon navigate to [p]rev file' })
+      vim.keymap.set('n', '<C-n>', ui.nav_prev, { desc = 'Harpoon navigate to [n]ext file' })
+    end,
+  },
+  -- DAP
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui', -- Optional: for a UI
+      'theHamsta/nvim-dap-virtual-text', -- Optional: for virtual text
+    },
+    config = function()
+      local dap = require 'dap'
+
+      -- Configure DAP for Go
+      dap.adapters.go = {
+        type = 'server',
+        port = 38697, -- Can be any port
+        executable = {
+          command = 'dlv',
+          args = { 'dap', '--accept-multiclient', '--headless', '-l', '127.0.0.1:38697' },
+        },
+      }
+
+      dap.configurations.go = {
+        {
+          type = 'go',
+          name = 'Debug',
+          request = 'launch',
+          mode = 'auto',
+          program = '${file}', -- This will debug the current file
+        },
+      }
+
+      -- Keybindings for DAP
+      vim.keymap.set('n', '<leader>d', function()
+        vim.cmd 'Ex'
+      end, { desc = 'View [d]irectory files' })
+      vim.api.nvim_set_keymap('n', '<F5>', ':lua require"dap".continue()<CR>', { desc = 'continue debug', noremap = true })
+      vim.api.nvim_set_keymap('n', '<F10>', ':lua require"dap".step_over()<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<F11>', ':lua require"dap".step_into()<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<F12>', ':lua require"dap".step_out()<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Leader>b', ':lua require"dap".toggle_breakpoint()<CR>', { noremap = true })
+    end,
+  },
+
+  --[[  {
+    'Jay-Madden/auto-fix-return.nvim',
+    config = function()
+      require('auto-fix-return').setup {}
+    end,
+  }, ]]
+}
